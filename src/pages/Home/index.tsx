fb11f21423474f4386739ce07b9fcd578014a5d2
Sathyajith <slakmal68@gmail.com>
2024-10-27 14:06:05 +0530

    loader text added

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -2,21 +2,26 @@ import NewsList from "../../components/NewsList";
 import MainLayout from "../../layouts/main";
 import { useNews } from "../../hooks/useNewsData";
 import NewsErrorBoundary from "../../ErrorBoundries/NewsErrorBoundry";
+import styles from "../../ErrorBoundries/NewsErrorBoundry/newsErrorBoundary.module.scss";
 
 /**
  * Using this additional component to wrap in an error boundary.
  */
 const NewsListData: React.FunctionComponent = () => {
-
   /**
    * Encapsulates the data fetching logic here. No significant usage of using react-query at this stage.
    * Bust make room for enhancements in the future
    */
-  const { data, error } = useNews("general");
+  const { data, error, isLoading } = useNews("general");
 
   if (error) {
     throw new Error("No news items available!"); // This will be caught in ErrorBoundary
   }
+
+  if (isLoading) {
+    return <h2 className={styles.message}>Loading...</h2>;
+  }
+
   return <>{data && <NewsList news={data.data} />}</>;
 };
 
